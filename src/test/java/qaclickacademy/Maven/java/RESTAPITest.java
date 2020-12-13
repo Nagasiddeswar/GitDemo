b3ba5a384aaf37b4a0fa9e8e9e9c13e5762e9ad6
Naga <s.snsreddy@gmail.com>
2020-12-13 19:28:12 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Maven/java/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Maven.java;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+   @Test
+	public void PostZira()
+	{
+		System.out.println("PostZira");
+}
+    @Test
+     public void deleteTwitter()
+
+{
+	  System.out.println("deleteTwitter");
+}
+}
